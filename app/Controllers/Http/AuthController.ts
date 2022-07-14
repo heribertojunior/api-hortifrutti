f24e9027f221bb964ceed2bfe40453a8636d82b8
HeribertoJunior <hsc.jr94@gmail.com>
2022-07-14 16:15:48 -0300

    fazendo seeds

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,31 @@
+import User from "App/Models/User";
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+  public async login({ auth, request, response}: HttpContextContract){
+      const email = request.input('email');
+      const password = request.input('password');
+
+      try {
+        const user =  await User.findByOrFail("email", email);
+        let expira;
+        switch(user.tipo){
+          case "clientes":
+            expira ="30days";
+            break;
+          case "estabelecimentos":
+            expira ="7days";
+            break;
+          case "admins":
+            expira ="1days";
+            break;
+          default:
+            expira="30days"
+          }
+          const token = await auth.use("api").attempt(email, password,{expiresIn: expira,  name: user.serialize().email,} );
+          response.ok(token);
+        }catch{
+        return response.badRequest("Invalid credentials");
+      }
+  }
+}
---
+++ database/seeders/Admin.ts
@@ -0,0 +1,17 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Admin from 'App/Models/Admin';
+import User from 'App/Models/User';
+
+export default class extends BaseSeeder {
+    public async run () {
+    const user = await User.create({
+      email: 'admin@email.com',
+      password: '123456',
+      tipo: 'admin',
+    });
+    await Admin.create({
+      nome: 'Admin1',
+      userId: user.id,
+    });
+  }
+}
