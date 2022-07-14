14b47420f0f71956d6ab74e8cc96375a8fca5f17
HeribertoJunior <hsc.jr94@gmail.com>
2022-07-14 15:03:54 -0300

    mais Models e migrations feitas

---
+++ database/migrations/1657821106630_categorias.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'categorias'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary();
+      table.string("nome").notNullable();
+      table.string("descricao").nullable();
+      table.string("posicao").notNullable();
+      table.boolean("ativo").notNullable().defaultTo(true);
+      table.integer('estabelecimento_id').unsigned().notNullable().references('id').inTable('estabelecimentos').onDelete('RESTRICT');
+      table.timestamps(true,true);
+      table.timestamp('deleted_at').nullable();
+
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
